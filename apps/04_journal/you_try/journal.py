d99da56b1466b40760d0bda5d0da07180fcf2539
sun-wukong <jasonc.scoon@gmail.com>
2022-03-02 18:45:01 -0500

    Updated files

--- apps/04_journal/you_try/journal.py
+++ apps/04_journal/you_try/journal.py
@@ -1,45 +1,43 @@
 from collections import deque
 from io import FileIO as file
 from time import sleep
-from tokenize import String
-from typing import OrderedDict
 
-journal_dir = "./data/"
-class Journal(OrderedDict):
-    def __init__(self,journal_name):
+journal_dir = "data/"
+
+
+class Journal():
+    def __init__(self, journal_name):
         self.name = journal_name
         self.entries = list()
         self.unsaved_entries = deque()
-        self.load(journal_name)
+        self.__load__(journal_name)
 
-    
     def __load__(self, journal_name):
         try:
-            with file.open("{}{}.jrn".format(journal_dir, journal_name), "r") as f:
-                for entry in f.read().strip(","):
-                    self.entries.append(entry)
+            with open("{}{}.jrn".format(journal_dir, journal_name), "r") as f:
+                for entry in f.readlines():
+                    self.entries.append(entry.strip("\n"))
         except FileNotFoundError:
-            print("File {}{}.jrn was not found".format(journal_dir, journal_name))
+            print("File {}{}.jrn was not found".format(
+                journal_dir, journal_name))
 
-
-    def add_entry(self,entry):  
+    def add_entry(self, entry):
         try:
-            if entry == None:
-                raise ValueError
-            elif type(entry) != String:
-                try:
-                    entry = str(entry)
-                except Exception:
-                    print("Invalid input")
-        except ValueError:
+            if entry is None:
+                raise Exception
+            else:
+                if(isinstance(entry, (int, float))):
+                    self.unsaved_entries.append("{}".format(entry))
+                else:
+                    self.unsaved_entries.append(entry)
+        except Exception:
             print("Invalid input")
-        finally:   
-            self.unsaved_entries.append(entry)
-
 
     def get_entries(self):
-        return self.entries
-        
+        return {
+            "entries": self.entries,
+            "unsaved": self.unsaved_entries
+        }
 
     def save(self):
         print("saving changes")
@@ -47,8 +45,8 @@ class Journal(OrderedDict):
             for entry in self.unsaved_entries[:2]:
                 # TODO: write in retry logic, if desriable
                 f.write(b"{},".format(entry))
-                print("Saved entry {}".format(self.unsaved_entries.index(entry, 0, -2) + 1))
+                print("Saved entry {}".format(
+                    self.unsaved_entries.index(entry, 0, -2) + 1))
             f.write(b"{}".format(self.unsaved_entries.pop()))
             print("saved last entry")
             sleep(3000)
-        
--- apps/04_journal/you_try/test.py
+++ apps/04_journal/you_try/test.py
@@ -10,12 +10,13 @@ test_entries = [
     None
 ]
 
+
 def run_evt_loop():
     pass
 
 
 def test_get_entries(journal_name, test_set):
-    #TODO: Create cases for empty journal and filled journal
+    # TODO: Create cases for empty journal and filled journal
     journal = Journal(journal_name)
     if len(journal.entries == 0):
         journal.entries = test_set
@@ -27,30 +28,49 @@ def test_get_entries(journal_name, test_set):
 
 
 def test_add_entry(journal, test_set):
-    journal.add_entry(test_set)
+    unsaved = journal.get_entries()["unsaved"]
     try:
-        assert(test_set in journal.entries == True)
+        if(test_set is None):
+            assert(test_set not in unsaved)
+        elif(type(test_set) == int or float):
+            assert str(test_set in unsaved)
+            print("Successfully added entry to jorunal:\n{}".format(test_set))
+        else:
+            assert(test_set in unsaved)
+            print("Successfully added entry to journal:\n{}".format(test_set))
     except AssertionError:
-        print("Entry {}\n was not added".format(test_set)) 
+        print("Test input {} failed".format(test_set))
 
 
 def test_load(journal_name):
     try:
         test_journal = Journal(journal_name)
+        entries = test_journal.get_entries()
+        assert_against = [str(item) for item in test_entries]
+        assert(len(entries) > 0)
+        assert(entries == assert_against)
+        print("Passed")
+    except AssertionError:
+        print("Test journal was not loaded properly")
     except FileNotFoundError:
         print("The {}.jrn file was not found".format(journal_name))
     pass
 
 
-def save():
+def test_save():
     pass
 
+
 if __name__ == '__main__':
-    print("Testing Journal.load")
-    test_load("test")
-    sleep(3)
-    # print("Testing Journal.add_entry")
+    # print("Testing Journal.load")
+    # test_load("test")
     # sleep(3)
+    print("Testing Journal.add_entry")
+    blank_jrn = Journal("blank")
+    for item in test_entries:
+        blank_jrn.add_entry(item)
+        test_add_entry(blank_jrn, item)
+    sleep(3)
     # print("Testing Journal.get_entries")
     # sleep(3)
     # print("Testing Journal.save")
